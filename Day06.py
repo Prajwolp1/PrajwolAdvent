bf0b406691f60aa0d770f29bf4ff8a7b1925b637
Prajwolp1 <145681394+Prajwolp1@users.noreply.github.com>
2024-12-11 13:18:17 -0500

    Update Day06.py
    Try 2

--- Day06.py
+++ Day06.py
@@ -10,6 +10,7 @@ count = 0
 posX = 0
 posY = 0
 facing = ""
+positions = []
 for i in range(len(file_data)):
     for j in range(len(file_data[i])):
         if (file_data[i][j] == "^"):
@@ -28,48 +29,22 @@ for i in range(len(file_data)):
             posY = i
             posX = j
             facing = "down"
-posX = int(posX) + 1
-posY = int(posY) + 1
+posX = int(posX)
+posY = int(posY)
 print(facing)
 print(posX)
 print(posY)
-while False:
-    if facing == "up":
-        print("up")
-        if (posY - 1 >= 0 and file_data[posX][posY] != "#"):
+while True:
+    if (facing == "up" and posY > 0):
+        if (file_data[posX][posY - 1] != "#"):
             posY -= 1
-            count += 1
-        if (file_data[posX][posY] == "#"):
+            if (file_data[posX][posY] != "X"):
+                count += 1
+            else:
+                file_data[posX][posY] == "X"
+        else:
             facing = "right"
-        if (posX - 1 < 0):
-            break
-    if facing == "down":
-        print("down")
-        if (posY + 1 < (len(file_data) - 1) and file_data[posX][posY] != "#"):
-            posY += 1
-            count += 1
-        if (file_data[posX][posY] == "#"):
-            facing = "right"
-        if (posY + 1 > (len(file_data) - 1)):
-            break
-    if facing == "right":
-        print("right")
-        if (posX + 1 < (len(file_data[0]) - 1) and file_data[posX][posY] != "#"):
-            posX += 1
-            count += 1
-        if (file_data[posX][posY] == "#"):
-            facing = "up"
-        if (posY + 1 > (len(file_data[0]) - 1)):
-            break
-    if facing == "left":
-        print("left")
-        if (posX - 1 >= 0 and file_data[posX][posY] != "#"):
-            posX -= 1
-            count += 1
-        if (file_data[posX][posY] == "#"):
-            facing = "down"
-        if (posY + 1 > (len(file_data[0]) - 1)):
-            break
-        
-print(count)
-                
+    else:
+        break
+    
+
