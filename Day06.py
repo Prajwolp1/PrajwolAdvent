819fb6a6a99a23271110108a03896caa8fd12af5
Prajwolp1 <145681394+Prajwolp1@users.noreply.github.com>
2024-12-17 13:18:23 -0500

    Update Day06.py

--- Day06.py
+++ Day06.py
@@ -5,6 +5,15 @@ def get_file_data(file_name):
         data.append(line.rstrip())
     return data
 
+def checkedValue(y_value, x_value):
+    for i in range(len(checkedY)):
+        if (checkedY[i] == y_value and checkedX[i] == x_value):
+            return False
+    return True
+    
+
+
+
 file_data = get_file_data("input.txt")
 count = 0
 posX = 0
@@ -32,29 +41,29 @@ for i in range(len(file_data)):
 posY = int(posY)
 posX = int(posX)
 end = False
+checkedY = []
+checkedX = []
 while not end:
     if facing == "up" and posY >= 0:
-        
         if (file_data[posY - 1][posX] != "#"):
-            if (file_data[posY - 1][posX] != "X"):
+            if (checkedValue(posY - 1, posX)):
                 count += 1
-            else:
-                file_data[posY - 1][posX] == "X"
+                checkedY.append(posY)
+                checkedX.append(posX)
             posY -= 1
         if (posY <= 0):
             end = True
         if not end:
             if (file_data[posY - 1][posX] == "#"):
                 facing = "right"
-
     if facing == "right" and posX < len(file_data[posY]) - 1:
-        
         if (file_data[posY][posX + 1] != "#"):
-            if (file_data[posY][posX + 1] != "X"):
+            if (checkedValue(posY, posX + 1)):
                 count += 1
-            else:
-                file_data[posY][posX + 1] == "X"
+                checkedY.append(posY)
+                checkedX.append(posX)
             posX += 1
+            
         if (posX >= len(file_data) - 1):
             end = True
         if not end:
@@ -63,11 +72,13 @@ while not end:
 
     if facing == "down" and posY < len(file_data):
         if (file_data[posY + 1][posX] != "#"):
-            if (file_data[posY + 1][posX] != "X"):
+            if (checkedValue(posY + 1, posX)):
                 count += 1
-            else:
-                file_data[posY + 1][posX] == "X"
+                checkedY.append(posY)
+                checkedX.append(posX)
             posY += 1
+            
+
         if (posY >= len(file_data) - 1):
             end = True
         if not end:
@@ -76,15 +87,17 @@ while not end:
 
     if facing == "left" and posX > 0:
         if (file_data[posY][posX - 1] != "#"):
-            if (file_data[posY][posX - 1] != "X"):
+            if (checkedValue(posY, posX + 1)):
                 count += 1
-            else:
-                file_data[posY][posX - 1] == "X"
+                checkedY.append(posY)
+                checkedX.append(posX)
             posX -= 1
+            
         if (posX <= 0):
             end = True
         if not end:
             if (file_data[posY][posX - 1] == "#"):
                 facing = "up"
+    
     
 print(count)
